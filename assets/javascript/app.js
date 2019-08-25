f29367329dcdd705db09d217a27dc77e64322839
cogitor <52011619+cogitor525@users.noreply.github.com>
2019-08-25 13:05:23 -0500

    started on game prestart display (shown on pageload); used hidden elements to maintain layout structure

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,29 @@
 // $('body').css('background-image', "url('assets/images/andromeda.jpg')");
 
+
+// these define duration of game timeouts
+const qTimeout = 10000;
+const qInterval = 3000;
+
+// these variables track the player's score/record
+let countCorrect;
+let countWrong;
+let countTimeout;
+
+// this object contains paths to images to be used in game
+const imageUrlSet = {
+    q1: "",
+    q2: "",
+    q3: "",
+    q4: "",
+    q5: "",
+    q6: "",
+    q7: "",
+    q8: "",
+    q9: "",
+    q10: "",
+};
+
 const questionArray = [];
 // questionArray will be array of objects with following structure:
 
@@ -14,19 +38,7 @@ const questionArray = [];
 //     imageUrl: "Image URL here"
 // }
 
-const imageUrlSet = {
-    q1: "",
-    q2: "",
-    q3: "",
-    q4: "",
-    q5: "",
-    q6: "",
-    q7: "",
-    q8: "",
-    q9: "",
-    q10: "",
-}
-
+// following function creates Question objects with above structure
 function createQuestion(question,correctAnswer,wrong1Answer,wrong2Answer,wrong3Answer,imageUrl) {
     this.question = question;
     this.answers = {};
@@ -35,17 +47,45 @@ function createQuestion(question,correctAnswer,wrong1Answer,wrong2Answer,wrong3A
     this.answers.wrong2 = wrong2Answer;
     this.answers.wrong3 = wrong3Answer;
     this.imageUrl = imageUrl;
-}
+};
 
+// following function calls above function to create new Question object,
+// then pushes to questionArray
 function addQuestion(question,correctAnswer,wrong1Answer,wrong2Answer,wrong3Answer,imageUrl) {
     const q = new createQuestion(question,correctAnswer,wrong1Answer,wrong2Answer,wrong3Answer,imageUrl);
     questionArray.push(q);
-}
+};
 
+// this section to create questionArray to be used in game
 addQuestion("Which particle mediates the EM force?","photon","electron","magneton","gluon","URL_here");
 addQuestion("Which newly developed procedure allows for precise genetic editing?","CRISpR","PCR","transcriptase","RNAi","URL_here");
+addQuestion("");
+addQuestion("");
+addQuestion("");
+addQuestion("");
+addQuestion("");
+addQuestion("");
+addQuestion("");
+addQuestion("");
+addQuestion("");
+addQuestion("");
 
-console.log(questionArray);
+// this function called when starting/restarting new game
+function initializeGame() {
+    countCorrect = 0;
+    countWrong = 0;
+    countTimeout = 0;
 
+};
 
+// GAME PRE-START: This section runs at pageload
 
+// $(".hidden").css('visibility', 'hidden');
+// #question {
+//     background-color: rgba(0,128,0,0.25);
+//     color: white;
+//     border: 1px solid green;
+//     margin-left: 160px;
+//     margin-right: 160px;
+//     margin-bottom: 25px;
+// }
