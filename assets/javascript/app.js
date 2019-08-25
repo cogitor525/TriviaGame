9088057db7536cf3c7974dcbed609c858287c079
cogitor <52011619+cogitor525@users.noreply.github.com>
2019-08-25 11:34:56 -0500

    array of question objects to be used for game can now be created with addQuestion(), which calls createQuestion() and then pushes the new object to questionArray

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,51 @@
-$(document).on("click", function(){
-    $('body').css('background-image', "url('assets/images/andromeda.jpg')");
-})
+// $('body').css('background-image', "url('assets/images/andromeda.jpg')");
+
+const questionArray = [];
+// questionArray will be array of objects with following structure:
+
+// questionArray[i] = {
+//     question: "Question here",
+//     answers: {
+//         correct: "Correct answer here",
+//         wrong1: "Wrong answer #1",
+//         wrong2: "Wrong answer #2",
+//         wrong3: "Wrong answer #3"
+//     },
+//     imageUrl: "Image URL here"
+// }
+
+const imageUrlSet = {
+    q1: "",
+    q2: "",
+    q3: "",
+    q4: "",
+    q5: "",
+    q6: "",
+    q7: "",
+    q8: "",
+    q9: "",
+    q10: "",
+}
+
+function createQuestion(question,correctAnswer,wrong1Answer,wrong2Answer,wrong3Answer,imageUrl) {
+    this.question = question;
+    this.answers = {};
+    this.answers.correct = correctAnswer;
+    this.answers.wrong1 = wrong1Answer;
+    this.answers.wrong2 = wrong2Answer;
+    this.answers.wrong3 = wrong3Answer;
+    this.imageUrl = imageUrl;
+}
+
+function addQuestion(question,correctAnswer,wrong1Answer,wrong2Answer,wrong3Answer,imageUrl) {
+    const q = new createQuestion(question,correctAnswer,wrong1Answer,wrong2Answer,wrong3Answer,imageUrl);
+    questionArray.push(q);
+}
+
+addQuestion("Which particle mediates the EM force?","photon","electron","magneton","gluon","URL_here");
+addQuestion("Which newly developed procedure allows for precise genetic editing?","CRISpR","PCR","transcriptase","RNAi","URL_here");
+
+console.log(questionArray);
+
+
+
